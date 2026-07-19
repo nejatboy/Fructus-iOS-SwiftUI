d33837e1b41ece4c3a306a92c9e2385ccdd58f1e
Nejat Boy <>
2026-07-19 22:11:16 +0300

    Project created.

---
+++ Fructus/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Fructus
+//
+//  Created by Nejat Boy on 19.07.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Nejat Boy on 19.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
